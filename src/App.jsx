3bbfafc2c5a8e012f99aa30b4045cf66c4d63dfc
n20014 <n20014@std.it-college.ac.jp>
2021-02-16 04:56:20 +0000

    jsx.css変更

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      Loaded: false,
+      items: []
+    }
+  }
+
+  componentDidMount () {
+    window
+      .fetch('https://aws.random.cat/meow')
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.rates)
+        this.setState({
+          Loaded: true,
+          images: json.file
+        })
+      })
+  }
+
+  render () {
+    const { Loaded, images } = this.state
+    console.log(this.state)
+    if (!Loaded) {
+      return <h1 Style='text'> 読み込み中です！！ </h1>
+    } else {
+      return (
+        <div>
+          <h1 Style='text'>MEOW</h1>
+          <img src={images} width='100%' height='800' />
+        </div>
+      )
+    }
+  }
+}
+
+export default App
